2347f8c818ede37733fa52195fb3891779d9dc9b
Igor Cezatte <igorcezatte13@gmail.com>
2020-11-09 23:40:19 -0300

    Search characters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,50 @@
-import { Title } from '../styles/pages/Home';
+import { useState } from 'react';
+import { CharacterList } from '../styles/pages/Home';
+
+import api from '../services/api';
+
+interface ICharacter {
+  id: string;
+  name: string;
+  gender: string;
+  image: string;
+}
+
+interface IData {
+  results: ICharacter[];
+  info: {
+    pages: string;
+  }
+}
 
 export default function Home() {
+  const [data, setData] = useState<IData>();
+  const [search, setSearch] = useState('');
+
+  function handleSearch() {
+      api.get(`/character/?name=${search}`).then((response) => {
+      setData(response.data);
+    });
+  }
+
   return (
     <div>
-      <Title>Hello Next</Title>
+      <input type="text" placeholder="Search" onChange={ e => setSearch(e.target.value) }/>
+      <button type="submit" onClick={handleSearch}>Pesquisar</button>
+
+        {data ? <CharacterList>
+          {data.results.map(character => {
+            return (
+              <li key={character.id}>
+                <h1>{character.name}</h1>
+                <img src={character.image}></img>
+              </li>
+            )
+          })}
+        </CharacterList> : null}
+
+        <div>{data ? data.info.pages : null}</div>
+
     </div>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-    color: #ff8c00;
+export const CharacterList = styled.ul`
+    margin-top: 150px;
+
+    padding: 10px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    list-style: none;
+
+    li {
+        display: flex;
+        flex-direction: column;
+
+        border-radius: 4px;
+        padding: 20px;
+
+        img {
+            align-self: center;
+            max-width: 250px;
+        }
+
+        > h1 {
+            font-size: 26px;
+            color: #ff8c00;
+            line-height: 20px;
+            align-self: center;
+            margin-bottom: 10px;
+        }
+    }
 `;
